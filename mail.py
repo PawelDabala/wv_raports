d00c6701fb65233568fa13d61dcfa5697b6e4d14
PawelDabala <pdabala@gmail.com>
2022-06-07 15:48:57 +0200

    Change app password

--- mail.py
+++ mail.py
@@ -1,18 +1,31 @@
-from datetime import date
+import os
+from datetime import date, timedelta
 
 from imap_tools import MailBox
 
-from seting import EMAIL_ADR, EMAIL_IMAP, EMAIL_PASSWD
+from seting import EMAIL_ADR, EMAIL_IMAP, EMAIL_APP_PASSWD
 
 # get all attachments from INBOX and save them to files
-msg_from = "pdabala@googlemail.com"
+msg_from = "lukasz.getter@phdmediadirection.com.pl"
 today_date = date.today()
+daybefore = date.today() - timedelta(days=5)
+directory = "attachments"
 
-with MailBox(EMAIL_IMAP).login(EMAIL_ADR, EMAIL_PASSWD, "INBOX") as mailbox:
+
+def remove_files(directory: str):
+    os.chdir(rf"{directory}")
+    all_files = os.listdir()
+    for f in all_files:
+        os.remove(f)
+
+
+remove_files(directory)
+os.chdir("..")
+
+with MailBox(EMAIL_IMAP).login(EMAIL_ADR, EMAIL_APP_PASSWD, "INBOX") as mailbox:
     for msg in mailbox.fetch():
-        if msg.from_ == msg_from and msg.date.date() == today_date:
-            print(msg.from_, msg.subject)
+        if msg.from_ == msg_from and msg.date.date() >= daybefore:
             for att in msg.attachments:
                 print("-", att.filename, att.content_type)
-                with open(f"attachments/{att.filename}", "wb") as f:
+                with open(rf"{directory}/{att.filename}", "wb") as f:
                     f.write(att.payload)
